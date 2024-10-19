2e9b48b77117c0e6b5da7d54b06046773685ca48
Ahmet Asaad Hammoud <a.h.ahmet.es@gmail.com>
2024-10-19 23:16:53 +0200

    done

--- config.php
+++ config.php
@@ -7,7 +7,8 @@ if (!defined('APP_PATH')) {
     define("APP_PATH", realpath(dirname(__FILE__)));
 }
 if (!defined('API_URL')) {
-    define("API_URL", $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . "/");
+    $scheme = isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http';
+    define("API_URL", $scheme . "://" . $_SERVER['HTTP_HOST'] . "/");
 }
 if (!defined('Excluded_Route')) {
     define("Excluded_Route",  []);
@@ -15,6 +16,5 @@ if (!defined('Excluded_Route')) {
 if (!defined('Get_Routes_With_Token')) {
     define("Get_Routes_With_Token",  []);
 }
-
 
 require APP_PATH . DS . "vendor" . DS . "autoload.php";
